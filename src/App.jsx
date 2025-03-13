9f70a7eae18069052cf3cae5eaf0f7464797b92e
pedroosorio <pedro.osorio04@inacapmail.cl>
2025-03-13 19:18:57 -0300

    inicio backend y frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,64 @@
+import styled, { ThemeProvider } from 'styled-components'
+import { } from './index'
+import { AuthContextProvider, MyRoutes, Light, Dark } from './index'
+import { createContext, useState } from 'react'
+import { Device } from './styles/breakpoints'
 
-import './App.css'
-import {} from './index'
-import { AuthContextProvider } from './index'
-import { MyRoutes } from './routers/routes'
+export const ThemeContext = createContext(null);
+
 function App() {
-
+  const [themeuse, setTheme] = useState('dark');
+  const theme = themeuse === "light" ? "light" : "dark";
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+  const themeStyle = theme === "light" ? Light : Dark
   return (
     <>
-      <AuthContextProvider>
-        <MyRoutes />
-      </AuthContextProvider>
+      <ThemeContext.Provider value={{ theme, setTheme }}>
+        <ThemeProvider theme={themeStyle}>
+          <AuthContextProvider>
+            <Container className={sidebarOpen ? "active" : ""}>
+              <section className='ContentSidebar'>Sidebar</section>
+              <section className='ContentMenuambur'>Menu Bar</section>
+              <section className='ContentRoutes'><MyRoutes /></section>
+            </Container>
+          </AuthContextProvider>
+        </ThemeProvider>
+      </ThemeContext.Provider>
     </>
   )
 }
 
-export default App
+const Container = styled.main`
+  display: grid;
+  grid-template-columns: 1fr;
+  background-color: ${({ theme }) => theme.bgtotal};
+  .ContentSidebar {
+    display: none;
+  }
+  .ContentMenuambur {
+    display: block;
+    position: absolute;
+    left: 20px;
+  }
+  @media ${Device.tablet} {
+    grid-template-columns: 65px 1fr;
+    &.active {
+      grid-template-columns: 220px 1fr;
+    }
+    .ContentSidebar {
+      display: initial;
+    }
+    .ContentMenuambur {
+      display: none;
+    }
+  }
+  .ContentRoutes {
+    grid-column: 1;
+    width: 100%;
+    @media ${Device.tablet} {
+      grid-column: 2;
+    }
+  }
+`;
+
+export default App;
--- src/components/template/HomeTemplate.jsx
+++ src/components/template/HomeTemplate.jsx
@@ -9,9 +9,37 @@ export function HomeTemplate() {
 }
 
 const Container = styled.div`
-  width: 100%;
   height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+  width: 100%;
+  background-color: ${(props) => props.theme.bgtotal};
+  color: ${({ theme }) => theme.text};
+  display: grid;
+  padding:15px;
+  grid-template:
+    "header" 100px
+    "area1" 100px
+    "area2" 100px
+    "main" auto;
+  .header {
+    grid-area: header;
+    background-color: rgba(103, 93, 241, 0.14);
+    display: flex;
+    align-items: center;
+  }
+  .area1 {
+    grid-area: area1;
+    background-color: rgba(229, 67, 26, 0.14);
+    display: flex;
+    align-items: center;
+  }
+  .area2 {
+    grid-area: area2;
+    background-color: rgba(77, 237, 106, 0.14);
+    display: flex;
+    align-items: center;
+  }
+  .main {
+    grid-area: main;
+    background-color: rgba(179, 46, 241, 0.14);
+  }
 `;
