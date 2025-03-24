c9bed4f4f22cf9d7b04dc17b4716e181215fcf88
Pedro Pablo Osorio San Martín <pedro.osorio04@inacapmail.cl>
2025-03-24 11:02:37 -0300

    creacion rutas y primera vista

---
+++ inventarios-pro/src/components/templates/HomeTemplate.jsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export function HomeTemplate() {
+    return (
+        <Container>
+            <h1>Home Page</h1>
+        </Container>
+    )
+}
+const Container = styled.div`
+
+`;
